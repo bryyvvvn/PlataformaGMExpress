5d0c21bfafd91c193c8da69f7e83d73b95d96e57
bryyvvvn <bryan.carvajal@alumnos.ucn.cl>
2026-05-11 18:55:44 -0400

    fix: arreglo dashboard

--- web/app/(admin)/dashboard/page.tsx
+++ web/app/(admin)/dashboard/page.tsx
@@ -14,10 +14,9 @@ export default function DashboardPage() {
         <div className="grid gap-6">
           {/* Aquí es donde irán las StatsCards y la Tabla que haremos después */}
           <div className="rounded-2xl border-2 border-dashed border-primary/30 bg-primary/5 p-12 text-center">
-            <h2 className="text-2xl font-bold text-primary">¡Estructura de Carpetas Lista!</h2>
+            <h2 className="text-2xl font-bold text-primary">¡Próximamente!</h2>
             <p className="mt-2 text-muted-foreground max-w-md mx-auto">
-              Si estás viendo esto, significa que el Layout (Sidebar azul) y el Header (barra superior) 
-              están cargando correctamente desde sus carpetas.
+              Se agregarán estadisticas claves para el administrador
             </p>
           </div>
         </div>
