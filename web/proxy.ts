2069630ddd3093c8a19a8b406f2a62de521fa473
JavierPizarroFleming6517 <javier.pizarro@alumnos.ucn.cl>
2026-07-01 19:58:25 -0400

    Update proxy.ts

--- web/proxy.ts
+++ web/proxy.ts
@@ -22,7 +22,7 @@ export default clerkMiddleware(async (auth, request) => {
       status: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
         'Access-Control-Allow-Credentials': 'true',
       },
